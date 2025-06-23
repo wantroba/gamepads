fc383930b7eac9d20dfd794930eaabf00151358a
ewerton.wantroba <wantr0ba@hotmail.com>
2025-06-23 11:49:02 -0300

    listGamepads method added. Rollback to use FlutterMethodChannel

--- packages/gamepads_ios/ios/Classes/GamepadsIosPlugin.swift
+++ packages/gamepads_ios/ios/Classes/GamepadsIosPlugin.swift
@@ -3,29 +3,25 @@ import GameController
 import UIKit
 
 public class GamepadsIosPlugin: NSObject, FlutterPlugin {
-  private var channel: FlutterEventChannel?
-  private var eventSink: FlutterEventSink?
-
-  // Map controllers to ids
+  private var channel: FlutterMethodChannel!
   private var controllerIds = [GCController: Int]()
   private var nextControllerId = 1
 
   public static func register(with registrar: FlutterPluginRegistrar) {
     let instance = GamepadsIosPlugin()
-    let messenger = registrar.messenger()
-    instance.channel = FlutterEventChannel(name: "xyz.luan/", binaryMessenger: messenger)
-    instance.channel?.setStreamHandler(instance)
+    instance.channel = FlutterMethodChannel(name: "xyz.luan/gamepads", binaryMessenger: registrar.messenger())
+    registrar.addMethodCallDelegate(instance, channel: instance.channel)
 
     NotificationCenter.default.addObserver(
       instance,
-      selector: #selector(controllerConnected),
+      selector: #selector(instance.controllerConnected),
       name: .GCControllerDidConnect,
       object: nil
     )
 
     NotificationCenter.default.addObserver(
       instance,
-      selector: #selector(controllerDisconnected),
+      selector: #selector(instance.controllerDisconnected),
       name: .GCControllerDidDisconnect,
       object: nil
     )
@@ -34,26 +30,29 @@ public class GamepadsIosPlugin: NSObject, FlutterPlugin {
       instance.setupController(controller)
     }
   }
-}
 
-extension GamepadsIosPlugin: FlutterStreamHandler {
-  public func onListen(withArguments arguments: Any?, eventSink events: @escaping FlutterEventSink) -> FlutterError? {
-    self.eventSink = events
-    return nil
+  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+    if call.method == "listGamepads" {
+      let gamepads = controllerIds.compactMap { (controller, id) -> [String: Any]? in
+        guard let vendorName = controller.vendorName else { return nil }
+        return [
+          "id": id,
+          "name": vendorName
+        ]
+      }
+      result(gamepads)
+    } else {
+      result(FlutterMethodNotImplemented)
+    }
   }
 
-  public func onCancel(withArguments arguments: Any?) -> FlutterError? {
-    self.eventSink = nil
-    return nil
-  }
-
-  @objc func controllerConnected(notification: Notification) {
+  @objc private func controllerConnected(notification: Notification) {
     if let controller = notification.object as? GCController {
       setupController(controller)
     }
   }
 
-  @objc func controllerDisconnected(notification: Notification) {
+  @objc private func controllerDisconnected(notification: Notification) {
     if let controller = notification.object as? GCController {
       controllerIds.removeValue(forKey: controller)
       // Optional: send disconnection event
@@ -69,19 +68,20 @@ extension GamepadsIosPlugin: FlutterStreamHandler {
     guard let gamepad = controller.extendedGamepad else { return }
     let gamepadId = controllerIds[controller]!
 
-    gamepad.dpad.valueChangedHandler = { [weak self] _, xValue, yValue in
-      self?.sendAnalogEvent(gamepadId: gamepadId, key: "dpad - xAxis", value: xValue)
-      self?.sendAnalogEvent(gamepadId: gamepadId, key: "dpad - yAxis", value: yValue)
+    // CORREÇÃO: dpad agora envia tipo "analog" e mapeia corretamente X e Y
+    gamepad.dpad.valueChangedHandler = { [weak self] element, xValue, yValue in
+      self?.sendAnalogEvent(gamepadId: gamepadId, key: "dpad - xAxis", value: xValue, element: element)
+      self?.sendAnalogEvent(gamepadId: gamepadId, key: "dpad - yAxis", value: yValue, element: element)
     }
 
-    gamepad.leftThumbstick.valueChangedHandler = { [weak self] _, xValue, yValue in
-      self?.sendAnalogEvent(gamepadId: gamepadId, key: "leftStick - xAxis", value: xValue)
-      self?.sendAnalogEvent(gamepadId: gamepadId, key: "leftStick - yAxis", value: yValue)
+    gamepad.leftThumbstick.valueChangedHandler = { [weak self] element, xValue, yValue in
+      self?.sendAnalogEvent(gamepadId: gamepadId, key: "leftStick - xAxis", value: xValue, element: element)
+      self?.sendAnalogEvent(gamepadId: gamepadId, key: "leftStick - yAxis", value: yValue, element: element)
     }
 
-    gamepad.rightThumbstick.valueChangedHandler = { [weak self] _, xValue, yValue in
-      self?.sendAnalogEvent(gamepadId: gamepadId, key: "rightStick - xAxis", value: xValue)
-      self?.sendAnalogEvent(gamepadId: gamepadId, key: "rightStick - yAxis", value: yValue)
+    gamepad.rightThumbstick.valueChangedHandler = { [weak self] element, xValue, yValue in
+      self?.sendAnalogEvent(gamepadId: gamepadId, key: "rightStick - xAxis", value: xValue, element: element)
+      self?.sendAnalogEvent(gamepadId: gamepadId, key: "rightStick - yAxis", value: yValue, element: element)
     }
 
     let buttons: [(GCControllerButtonInput?, String)] = [
@@ -96,29 +96,29 @@ extension GamepadsIosPlugin: FlutterStreamHandler {
     ]
 
     for (button, name) in buttons {
-      button?.valueChangedHandler = { [weak self] _, _, pressed in
-        self?.sendButtonEvent(gamepadId: gamepadId, key: name, value: pressed ? 1.0 : 0.0)
+      button?.valueChangedHandler = { [weak self] element, _, pressed in
+        self?.sendButtonEvent(gamepadId: gamepadId, key: name, value: pressed ? 1.0 : 0.0, element: element)
       }
     }
   }
 
-  private func sendAnalogEvent(gamepadId: Int, key: String, value: Float) {
-    eventSink?([
+  private func sendAnalogEvent(gamepadId: Int, key: String, value: Float, element: GCControllerElement) {
+    channel.invokeMethod("event", arguments: [
       "type": "analog",
       "gamepadId": gamepadId,
       "key": key,
       "value": value,
-      "time": Int(Date().timeIntervalSince1970)
+      "time": Int(element.timestamp)
     ])
   }
 
-  private func sendButtonEvent(gamepadId: Int, key: String, value: Float) {
-    eventSink?([
+  private func sendButtonEvent(gamepadId: Int, key: String, value: Float, element: GCControllerElement) {
+    channel.invokeMethod("event", arguments: [
       "type": "button",
       "gamepadId": gamepadId,
       "key": key,
       "value": value,
-      "time": Int(Date().timeIntervalSince1970)
+      "time": Int(element.timestamp)
     ])
   }
 }
